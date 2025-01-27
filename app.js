9acf8e23a8d5ebe79a58534f52a0e43175849ee5
polyglisdev <ramimarinette@gmail.com>
2025-01-27 15:02:18 -0400

    se agregaron correcioenes a la formula para sortear un amigo

---
+++ app.js
@@ -0,0 +1,97 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let userName = `¿Cuál es tu nombre?`;
+
+// Declarar un array para almacenar los elementos
+let amigos = [];
+
+// Funcion declarativa para agregar un elemento
+function addFriend() {
+
+// obteniendo el input y el valor
+    const nuevoAmigo = document.querySelector('#nombreAmigo');
+    const valor = nuevoAmigo.value.trim();
+
+// validar que el campo no este vacio
+    if(valor) {
+// añadir valor al array
+    amigos.push(valor);
+
+// Actualizar la lista en el DOM
+actualizarlista();
+
+// Limpiar input
+nuevoAmigo.value = '';
+} else {
+    alert('Error!!! Por favor ingresa el nombre un de amigo.');
+}
+
+// Enfoca el input nuevamente
+    nuevoAmigo.focus();
+
+}
+
+// Función declarativa para actualizar lista del DOM
+function actualizarlista() {
+
+// Obtener el elemento UL
+const lista = document.querySelector('#listaAmigos');
+
+//limpiar la lista
+lista.innerHTML = '';
+
+// Recorrer el array y crear los elementos <li>
+    for( let amigo of amigos) {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+// Agregar evento al botón 
+ document.getElementById('button-add').addEventListener('click', addFriend);
+
+
+
+function sortearAmigo(){
+
+   const resultado = document.getElementById('resultado');
+   
+   if(amigos.length > 0 ){ 
+   // Generar indice aletorio basado en la longitud el array
+   const aleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[aleatorio]; 
+    
+// Mostrar el amigo sorteado en el DOM
+   resultado.textContent = `El amigo sorteao es: ${amigoSorteado}`
+    }else {        
+    resultado.textContent = `Por favor, ingrese nombres de amigo suyos.`;
+    }
+}
+document.getElementById('button-draw').addEventListener('click', sortearAmigo);
+
+
+
+
+
+
+
+// validar que el campo no este vacio
+// function validarCampo(){
+//     let campo = document.querySelector('#amigo').value;
+//     if (campo === "");
+//     {
+//     }
+// }
+
+function Saludo(){
+    // Establecer las condiciones para "día", "tarde" y "noche"
+    if (hora >= 6 && hora < 12) {
+        parteDelDia = "Buenos días";
+    } else if (hora >= 12 && hora < 18) {
+        parteDelDia = "Buenas tardes";
+    } else {
+        parteDelDia = "Buenas noches";
+    }
+    // Imprimir el resultado
+    console.log(parteDelDia);
+}
